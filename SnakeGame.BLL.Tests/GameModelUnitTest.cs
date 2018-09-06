bc3800aa31bff42003f3f8170407d1d3d509460a
dmitrybeloff <39600725+dmitrybeloff@users.noreply.github.com>
2018-09-06 14:18:44 +0300

    Добавил тесты

---
+++ SnakeGame.BLL.Tests/GameModelUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakeGame.BLL.Models;
+
+namespace SnakeGame.BLL.Tests
+{
+    /// <summary>
+    /// Сводное описание для UnitTest1
+    /// </summary>
+    [TestClass]
+    public class GameModelUnitTest
+    {      
+        public GameModelUnitTest()
+        {
+            
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void PlayersHaveDifferentTypesTest()
+        {
+            GameModel gameModel = new GameModel(new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() },
+                new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() });
+            Assert.AreNotEqual(gameModel.PlayerList[0].PlayerType, gameModel.PlayerList[1].PlayerType);
+        }
+    }
+}
---
+++ SnakeGame.BLL.Tests/GameServiceUnitTest.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakeGame.BLL.Enum;
+using SnakeGame.BLL.Models;
+using SnakeGame.BLL.Services;
+
+namespace SnakeGame.BLL.Tests
+{
+    [TestClass]
+    public class GameServiceUnitTest
+    {
+        [TestMethod]
+        public void PlayerChangeTest()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage;
+
+            var previousId = gameModel.CurrentId;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+
+            Assert.AreNotEqual(previousId, gameModel.CurrentId);
+        }
+
+        [TestMethod]
+        public void TestErrorResult()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+
+            var result = gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+
+            Assert.AreEqual(result, GameMoveResults.Error);
+        }
+
+        [TestMethod]
+        public void TestErrorResultMessage()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+
+            var result = gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+
+            Assert.AreEqual(gameMessage.Contains("Ошибка"), true);
+        }
+
+        [TestMethod]
+        public void TestDrawResult()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString() };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 1, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 2, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 3, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 4, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 6, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 5, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 8, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 7, out gameMessage);
+
+            Assert.AreEqual(gameMessage.Contains("Ничья"), true);
+        }
+
+        [TestMethod]
+        public void GameLogicRowTest()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p1" };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p2" };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage = string.Empty;
+
+            var winnerUsername = gameModel.CurrentUsername;
+
+            GameMoveResults result;
+
+            for (var i = 0; i < 9; i+=3)
+            {
+                int tmp = i < 6 ? i + 3 : i - 6;
+                for (var j = 0; j < i+3; j ++)
+                {               
+                    result = gameService.MakeMove(ref gameModel, gameModel.CurrentId, i + j, out gameMessage);
+                    if (result == GameMoveResults.EndGame)
+                        break;
+                    gameService.MakeMove(ref gameModel, gameModel.CurrentId, tmp + j, out gameMessage);
+                }
+
+                Assert.AreEqual(gameMessage.Contains(winnerUsername), true);
+
+                gameModel = new GameModel(p1, p2);
+                winnerUsername = gameModel.CurrentUsername;
+            }
+        }
+
+        [TestMethod]
+        public void GameLogicColumnTest()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p1" };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p2" };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage = string.Empty;
+
+            var winnerUsername = gameModel.CurrentUsername;
+
+            GameMoveResults result;
+
+            for (var i = 0; i < 3; i++)
+            {
+                int tmp = i < 2 ? i + 1 : i - 2;
+                for (var j = 0; j < 9; j += 3)
+                {
+                    result = gameService.MakeMove(ref gameModel, gameModel.CurrentId, i + j, out gameMessage);
+                    if (result == GameMoveResults.EndGame)
+                        break;
+                    gameService.MakeMove(ref gameModel, gameModel.CurrentId, tmp + j, out gameMessage);
+                }
+
+                Assert.AreEqual(gameMessage.Contains(winnerUsername), true);
+
+                gameModel = new GameModel(p1, p2);
+                winnerUsername = gameModel.CurrentUsername;
+            }
+        }
+
+        [TestMethod]
+        public void GameLogicX1Test()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p1" };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p2" };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage = string.Empty;
+
+            var winnerUsername = gameModel.CurrentUsername;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 1, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 4, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 2, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 8, out gameMessage);
+
+            Assert.AreEqual(gameMessage.Contains(winnerUsername), true);
+        }
+
+        [TestMethod]
+        public void GameLogicX2Test()
+        {
+            GameService gameService = new GameService();
+
+            var p1 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p1" };
+            var p2 = new TicaTacToeUserModel { Id = Guid.NewGuid().ToString(), Username = "p2" };
+
+            GameModel gameModel = new GameModel(p1, p2);
+
+            string gameMessage = string.Empty;
+
+            var winnerUsername = gameModel.CurrentUsername;
+
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 2, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 0, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 4, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 1, out gameMessage);
+            gameService.MakeMove(ref gameModel, gameModel.CurrentId, 6, out gameMessage);
+
+            Assert.AreEqual(gameMessage.Contains(winnerUsername), true);
+        }
+    }
+}
